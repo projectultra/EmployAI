f19f571a5e55120e14dbf8d41aef5e0cd8d84b02
tornado-hub <diasdickson93@gmail.com>
2024-04-16 10:06:57 +0530

    modified the index.html

--- app.py
+++ app.py
@@ -43,5 +43,9 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
